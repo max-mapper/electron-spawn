65b6193ced68306ce887549b7e6f3626492a8528
Max Ogden <max@maxogden.com>
2015-04-28 13:53:34 -0700

    allow passing all args in

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ app.on('ready', function () {
   var win = new BrowserWindow({show: false})
   win.loadUrl('file://' + path.join(__dirname, 'index.html'))  
   win.webContents.on('did-finish-load', function() {
-    win.webContents.send('module', path.join(process.cwd(), process.argv[2]))
+    win.webContents.send('args', process.argv)
   })
 })
 
