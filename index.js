8653d9efbdfefd9e05b0efc1e22a79a841c62529
Tucker Whitehouse <TuckerWhitehouse@gmail.com>
2015-08-04 11:23:21 -0400

    Use array.prototype.pop to extract the last item

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ module.exports = function () {
   var scripts = Array.prototype.slice.call(arguments)
   var opts = {}
   if (typeof scripts[scripts.length - 1] !== 'string') {
-    opts = scripts.slice(-1)[0]
-    scripts = scripts.slice(0, -1)
+    opts = scripts.pop()
   }
   var cmd = ['electron', path.resolve(__dirname, 'cli.js')].concat(scripts)
   return spawn(cmd.join(' '), opts)
