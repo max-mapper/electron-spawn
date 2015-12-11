64354a551b5283a1aefc2758891fb9f775049525
Nick Niemeir <nick.niemeir@gmail.com>
2015-12-11 14:53:39 -0800

    Fix for latest electron api.

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ var app = require('app')
 var BrowserWindow = require('browser-window')
 app.on('ready', function () {
   win = new BrowserWindow({show: false})
-  win.loadUrl('file://' + path.join(__dirname, 'index.html'))  
+  win.loadURL('file://' + path.join(__dirname, 'index.html'))  
   win.webContents.on('did-finish-load', function() {
     win.webContents.send('args', process.argv)
   })
