8fcc5bd67955251320689d7f0037bd51dd21f604
James Halliday <mail@substack.net>
2016-02-07 14:13:05 -0800

    simpler way to set process.argv

--- cli.js
+++ cli.js
@@ -5,8 +5,7 @@ var app = require('app')
 var BrowserWindow = require('browser-window')
 app.on('ready', function () {
   win = new BrowserWindow({show: false})
-  win.loadURL('file://' + path.join(__dirname, 'index.html')
-    + '#' + encodeURIComponent(JSON.stringify(process.argv.slice(1))))
+  win.loadURL('file://' + path.join(__dirname, 'index.html'))
   win.webContents.on('did-finish-load', function() {
     win.webContents.send('args', process.argv)
   })
